7bfc3b237a8c1ccf23254673c859cab10ad193a3
Danh Trọng Trần <danhtrongtran@192.168.1.36>
2025-08-07 17:16:00 +0700

    push - 2025-08-07 17:16:00

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps: [
+    {
+      name: 'iqx',
+      script: 'npm',
+      args: 'start',
+      cwd: './',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3031
+      },
+      env_development: {
+        NODE_ENV: 'development',
+        PORT: 3031
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3031
+      },
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true
+    }
+  ]
+};
